a3d92337cf308f89726d7713069d78aacc23148b
w2ak <w2ak@users.noreply.github.com>
2017-09-19 13:00:29 +0200

    [main.cpp] polymorphism
    Implemented test to test polymorphism and different
    open situations.
    
    Signed-off-by: w2ak <w2ak@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -16,19 +16,24 @@ using namespace std;
 
 /**
  * \fn int main (void)
- * \brief Hello World
+ * \brief Creates, opens and deletes a list of Multimedia objects.
  * \return 0
  */
 int main() {
+  Multimedia ** m = new Multimedia * [6];
   stringstream s;
-  Picture * p = new Picture();
-  p->print(s);
-  cerr << s.str() << endl;
-  p->open();
-  s.str(string());
-  Video * v = new Video();
-  v->print(s);
-  cerr << s.str() << endl;
-  v->open();
+  m[0] = new Video("small vid","small.mp4",180);                  // Existing video
+  m[1] = new Picture("small pic","small.jpg",48.856638,2.352241); // Existing picture
+  m[2] = new Video("null vid","null.mp4",42);                     // Non-existing video
+  m[3] = new Picture("null pic","null.jpg",13.37,3.14159265);     // Non-existing picture
+  m[4] = new Video();                                             // Null video
+  m[5] = new Picture();                                           // Null picture
+  for (int i=0; i<6; i++) {
+    s.str(string());
+    m[i]->print(s);
+    cerr << i << ": " << s.str() << endl;
+    m[i]->open();
+  }
+  delete[] m;
   return 0;
 }
