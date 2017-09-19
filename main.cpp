73acc7070a72c8aa78c0c2dddb2958cd87c6260a
w2ak <w2ak@users.noreply.github.com>
2017-09-19 12:34:23 +0200

    [main.cpp] creation
    Signed-off-by: w2ak <w2ak@users.noreply.github.com>

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/**
+ * \file main.cpp
+ * \brief Opens a list of media files.
+ * \author Clement Durand <clement.durand@telecom-paristech.fr>
+ * \version 1.0
+ * \date 2017-09-19
+ *
+ * Test program for the implementation of the Multimedia classes.
+ *
+ */
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+/**
+ * \fn int main (void)
+ * \brief Hello World
+ * \return 0
+ */
+int main() {
+  cout << "Hello world" << endl;
+  return 0;
+}
