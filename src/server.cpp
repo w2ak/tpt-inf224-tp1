b5e8377a3acd2bc8231eb206167d9ed0a6a4dd32
w2ak <w2ak@users.noreply.github.com>
2017-10-11 15:20:37 +0200

    [server.cpp] implemented commands
    Signed-off-by: w2ak <w2ak@users.noreply.github.com>

--- src/server.cpp
+++ src/server.cpp
@@ -46,27 +46,76 @@ public:
    */
   bool processRequest(TCPConnection& cnx, const string& request, string& response)
   {
-    cerr << "\nRequest: '" << request << "'" << endl;
+    int ok = 0;
+    string help = "<fileInfo|groupInfo|play> <media-name>";
+    stringstream resp;
+    stringstream rqst;
+    rqst.str(request);
+
+    cerr << "\nRequest: '" << rqst.str() << "'" << endl;
 
     // 1) pour decouper la requête:
     // on peut par exemple utiliser stringstream et getline()
-    
-    
+    char sep = ' ';
+    string cmd;
+    string name;
+
+    // Get the two parameters
+    if (!rqst.rdbuf()->in_avail()) { resp << help; ok = 2; goto done; }
+    getline(rqst,cmd,sep);
+    if (!rqst.rdbuf()->in_avail()) { resp << help; ok = 2; goto done; }
+    getline(rqst,name,sep);
+    if (rqst.rdbuf()->in_avail()) { resp << help; ok = 2; goto done; }
+
     // 2) faire le traitement:
     // - si le traitement modifie les donnees inclure: TCPLock lock(cnx, true);
     // - sinon juste: TCPLock lock(cnx);
+    if (cmd == "fileInfo") {
+      if (lib.hasFile(name)) {
+        lib.fileInfo(name,resp);
+      } else {
+        ok = 1; resp << "Not Found";
+      }
+      goto done;
+    }
+    if (cmd == "groupInfo") {
+      if (lib.hasGroup(name)) {
+        lib.groupInfo(name,resp);
+      } else {
+        ok = 1; resp << "Not Found";
+      }
+      goto done;
+    }
+    if (cmd == "play") {
+      if (lib.hasFile(name)) {
+        resp << "Now playing: ";
+        lib.fileInfo(name,resp);
+        lib.open(name);
+      } else {
+        ok = 1; resp << "Not Found";
+      }
+      goto done;
+    }
+    resp << help; ok = 2; goto done;
 
 
     // 3) retourner la reponse au client:
-    // - pour l'instant ca retourne juste OK suivi de la requête
-    // - pour retourner quelque chose de plus utile on peut appeler la methode print()
-    //   des objets ou des groupes en lui passant en argument un stringstream
-    // - attention, la requête NE DOIT PAS contenir les caractères \n ou \r car
-    //   ils servent à délimiter les messages entre le serveur et le client
-    
-    response = "OK: " + request;
+done:
+    switch (ok) {
+      case 0:
+        response = "OK: "; break;
+      case 1:
+        response = "KO: "; break;
+      case 2:
+        response = "??: "; break;
+      default:
+        response = "ShouldNotHappen "; break;
+    }
+
+    response+=resp.str();
+
     cerr << "response: " << response << endl;
-    
+
     // renvoyer false si on veut clore la connexion avec le client
     return true;
   }
